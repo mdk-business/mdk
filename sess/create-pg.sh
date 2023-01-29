7c3c9bbc2e3793a379d72040b7bb8e93abde30ff
MDK.business <122910955+mdk-business@users.noreply.github.com>
2023-01-29 17:41:45 +0200

    Update create-pg.sh

--- sess/create-pg.sh
+++ sess/create-pg.sh
@@ -5,24 +5,27 @@ readonly DIR_PM_VOL="$HOME/.pv"
 readonly DIR_PG_VOL="$DIR_PM_VOL/pg"
 readonly DIR_PG_D01="$DIR_PM_VOL/pg"
 
-echo "Installing PostgreSQL server as podmain container.."
-echo "You are running this script as $USER"
+echo -e "Installing PostgreSQL server as podmain container.."
+echo -e "You are running this script as $USER"
 
-echo "Creating directory for podman volumes.."
+echo -e "\nCreating directory for podman volumes.."
 mkdir -p "$DIR_PM_VOL"
 touch "$DIR_PM_VOL/-directory-of-podman-volumes"
 
-echo "Creating directory for postgres volumes.."
+echo -e "\nCreating directory for postgres volumes.."
 mkdir -p "$DIR_PG_VOL"
-touch -e "$DIR_PG_VOL/-directory-of-postgres-volumes"
+touch "$DIR_PG_VOL/-directory-of-postgres-volumes"
 
-echo "Creating directory for postgres data.."
+echo -e "\nCreating directory for postgres data.."
 mkdir -p "$DIR_PG_D01"
-echo "For data volume is used dir '$DIR_PG_D01'"
-touch -e "$DIR_PG_D01/-directory-of-postgres-data"
+echo -e "For data volume is used dir '$DIR_PG_D01'"
+touch "$DIR_PG_D01/-directory-of-postgres-data"
 
-echo "Pulling latest official PostgreSQl image"
+echo -e "\nPulling latest official PostgreSQl image"
 podman pull docker.io/library/postgres:latest
 
-echo "Pulling latest official PgAdmin image"
+echo -e "\nPulling latest official PgAdmin image"
 podman pull docker.io/dpage/pgadmin4:latest
+
+echo -e "\nList of podman images:"
+podman images
