2e13f5073afbdda59ba49f69a9b26d629b98d071
MDK.business <122910955+mdk-business@users.noreply.github.com>
2023-01-29 23:28:08 +0200

    Update create-pg.sh

--- sess/create-pg.sh
+++ sess/create-pg.sh
@@ -1,10 +1,5 @@
 ##!/bin/bash
 
-# Defaults
-readonly DIR_PM_VOL="$HOME/.pv"
-readonly DIR_PG_VOL="$DIR_PM_VOL/pg"
-readonly DIR_PG_D01="$DIR_PG_VOL/$(date +%s%3N)"
-
 # Go to home folder
 cd "$HOME"
 
@@ -31,22 +26,6 @@ if [ $pgAdminExists == 0 ]; then
 	exit
 fi
 
-# Create data directory
-echo -e "Installing PostgreSQL server as podman container.."
-echo -e "You are running this script as $USER"
-
-echo -e "\nCreating directory for podman volumes.."
-mkdir -p "$DIR_PM_VOL"
-touch "$DIR_PM_VOL/-podman-volumes"
-
-echo -e "\nCreating directory for postgres volumes.."
-mkdir -p "$DIR_PG_VOL"
-touch "$DIR_PG_VOL/-postgres-volumes"
-
-echo -e "\nCreating directory for postgres data.."
-mkdir -p "$DIR_PG_D01"
-echo -e "\t Data directory: $DIR_PG_D01"
-
 ## Pull images
 echo -e "\nPulling latest official PostgreSQl image.."
 podman pull docker.io/library/postgres:latest
@@ -62,10 +41,10 @@ read -p "Enter password for new PostgreSQL server: " -s userProvidedPSQLPswd
 
 # Create container
 echo -e "\nCreating PostgreSQl container.."
-podman create --name postgres \
+podman create \
+	--name postgres \
 	-p 5432:5432 \
 	-e POSTGRES_PASSWORD=$userProvidedPSQLPswd \
-	-v "$DIR_PG_D01":/var/lib/postgresql/data:Z \
 	postgres:latest
 
 unset userProvidedPSQLPswd
@@ -76,3 +55,27 @@ podman start postgres
 
 echo -e "\nStatus of postgres:"
 podman ps -f name=postgres
+
+
+echo -e "\Please provide data for PgAdmin server administrator.."
+
+echo ""
+read -p "E-mail : " userProvidedPgAdminEmail
+read -p "Password : " -s userProvidedPgAdminLPswd
+
+# Create container
+echo -e "\nCreating PgAdmin container.."
+podman create \
+	--name pgadmin \
+	-p 9180:80 \
+	-p 9183:443 \
+	-e PGADMIN_DEFAULT_EMAIL=$userProvidedPgAdminEmail \
+	-e PGADMIN_DEFAULT_PASSWORD=$userProvidedPgAdminLPswd \
+	pgadmin4:latest
+
+## Run container
+echo -e "\nStarting PgAdmin container.."
+podman start pgadmin
+
+echo -e "\nStatus of pgadmin4:"
+podman ps -f name=pgadmin
